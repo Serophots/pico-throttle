4254604fe78c7a355bff135aa340f6fde9c3753c
Serophots <tomtidbury07@gmail.com>
2026-07-20 12:58:51 +0100

    improve HardwarePins read logic

--- src/driver.rs
+++ src/driver.rs
@@ -53,65 +53,31 @@ impl<'d> HardwarePins<'d> {
             unused_7,
         } = self;
 
-        if throttle_disc_0.read() {
-            buttons |= HardwareButtons::THROTTLE_DISC_0;
-        }
-        if throttle_disc_1.read() {
-            buttons |= HardwareButtons::THROTTLE_DISC_1;
-        }
-        if throttle_toga_0.read() {
-            buttons |= HardwareButtons::THROTTLE_TOGA_0;
-        }
-        if throttle_toga_1.read() {
-            buttons |= HardwareButtons::THROTTLE_TOGA_1;
-        }
-        if eng_master_0.read() {
-            buttons |= HardwareButtons::ENG_MASTER_0;
-        }
-        if eng_master_1.read() {
-            buttons |= HardwareButtons::ENG_MASTER_1;
-        }
-        if eng_reverse_0.read() {
-            buttons |= HardwareButtons::ENG_REVERSE_0;
-        }
-        if eng_reverse_1.read() {
-            buttons |= HardwareButtons::ENG_REVERSE_1;
-        }
-        if ignition_crank.read() {
-            buttons |= HardwareButtons::IGNITION_CRANK;
-        }
-        if ignition_norm.read() {
-            buttons |= HardwareButtons::IGNITION_NORM;
-        }
-        if ignition_start.read() {
-            buttons |= HardwareButtons::IGNITION_START;
-        }
-        if parking_brake.read() {
-            buttons |= HardwareButtons::PARKING_BRAKE;
-        }
-        if unused_0.read() {
-            buttons |= HardwareButtons::UNUSED_0;
-        }
-        if unused_1.read() {
-            buttons |= HardwareButtons::UNUSED_1;
-        }
-        if unused_2.read() {
-            buttons |= HardwareButtons::UNUSED_2;
-        }
-        if unused_3.read() {
-            buttons |= HardwareButtons::UNUSED_3;
-        }
-        if unused_4.read() {
-            buttons |= HardwareButtons::UNUSED_4;
-        }
-        if unused_5.read() {
-            buttons |= HardwareButtons::UNUSED_5;
-        }
-        if unused_6.read() {
-            buttons |= HardwareButtons::UNUSED_6;
-        }
-        if unused_7.read() {
-            buttons |= HardwareButtons::UNUSED_7;
+        for (button, hardware_button) in [
+            (throttle_disc_0, HardwareButtons::THROTTLE_DISC_0),
+            (throttle_disc_1, HardwareButtons::THROTTLE_DISC_1),
+            (throttle_toga_0, HardwareButtons::THROTTLE_TOGA_0),
+            (throttle_toga_1, HardwareButtons::THROTTLE_TOGA_1),
+            (eng_master_0, HardwareButtons::ENG_MASTER_0),
+            (eng_master_1, HardwareButtons::ENG_MASTER_1),
+            (eng_reverse_0, HardwareButtons::ENG_REVERSE_0),
+            (eng_reverse_1, HardwareButtons::ENG_REVERSE_1),
+            (ignition_crank, HardwareButtons::IGNITION_CRANK),
+            (ignition_norm, HardwareButtons::IGNITION_NORM),
+            (ignition_start, HardwareButtons::IGNITION_START),
+            (parking_brake, HardwareButtons::PARKING_BRAKE),
+            (unused_0, HardwareButtons::UNUSED_0),
+            (unused_1, HardwareButtons::UNUSED_1),
+            (unused_2, HardwareButtons::UNUSED_2),
+            (unused_3, HardwareButtons::UNUSED_3),
+            (unused_4, HardwareButtons::UNUSED_4),
+            (unused_5, HardwareButtons::UNUSED_5),
+            (unused_6, HardwareButtons::UNUSED_6),
+            (unused_7, HardwareButtons::UNUSED_7),
+        ] {
+            if button.read() {
+                buttons |= hardware_button;
+            }
         }
 
         buttons
