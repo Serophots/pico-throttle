44c6ff8b085a45a2609074f399a7493f151518bc
Serophots <tomtidbury07@gmail.com>
2026-07-23 22:14:08 +0100

    fix :->

--- src/tasks/input.rs
+++ src/tasks/input.rs
@@ -70,8 +70,8 @@ pub async fn input_task(
             // Note ^: Bunch reads on the same axis so the
             // multiplexer has to switch channels less often
 
-            buttons = (buttons | ((axis0_status.bits() as u32) >> 2))
-                | ((axis1_status.bits() as u32) << 1);
+            buttons =
+                (buttons | ((axis0_status.bits() as u32) >> 3)) | (axis1_status.bits() as u32);
 
             HardwareDescriptor {
                 axis0,
